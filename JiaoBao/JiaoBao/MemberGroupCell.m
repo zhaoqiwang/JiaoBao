17cdfd349f41b31126a2173e8d5f3c8929cfdde7
songyanming <1062608083@qq.com>
2015-04-23 16:04:12 +0800

    cell

---
+++ JiaoBao/JiaoBao/MemberGroupCell.m
@@ -0,0 +1,23 @@
+//
+//  MemberGroupCell.m
+//  JiaoBao
+//
+//  Created by songyanming on 15/4/23.
+//  Copyright (c) 2015年 JSY. All rights reserved.
+//
+
+#import "MemberGroupCell.h"
+
+@implementation MemberGroupCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
