95287c4e1d9a226bd73e17d49afc7d7eefde8772
songyanming <1062608083@qq.com>
2015-04-23 11:03:57 +0800

    发表动态与发布分享

--- JiaoBao/JiaoBao/views/InternetApplications/exchange/DetailQueryViewController.h
+++ JiaoBao/JiaoBao/views/InternetApplications/exchange/DetailQueryViewController.h
@@ -15,8 +15,8 @@
 @interface DetailQueryViewController : UIViewController<MyNavigationDelegate>
 @property (nonatomic,strong) MyNavigationBar *mNav_navgationBar;//导航条
 @property(nonatomic,strong)NSString *selectedDateStr;
-@property (weak, nonatomic) IBOutlet UILabel *selectedDate;
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UILabel *selectedDate;//已选日期
+@property (weak, nonatomic) IBOutlet UITableView *tableView;//查询到的日程记录列表
 
 
 @end
