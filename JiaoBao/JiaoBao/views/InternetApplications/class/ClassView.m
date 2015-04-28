324ef8d663b86cdea0aba504f2ef326a13fa77cd
Zqw <578159259@qq.com>
2015-04-28 18:12:32 +0800

    事务界面、学校圈界面，单位显示会遮挡问题

--- JiaoBao/JiaoBao/views/InternetApplications/class/ClassView.m
+++ JiaoBao/JiaoBao/views/InternetApplications/class/ClassView.m
@@ -473,8 +473,9 @@
         }
         cell.mLab_class.hidden = NO;
     }
-    CGSize unitSize = [tempUnit sizeWithFont:[UIFont systemFontOfSize:14]];
-    cell.mLab_class.frame = CGRectMake(cell.mLab_name.frame.origin.x+cell.mLab_name.frame.size.width, 18, unitSize.width, cell.mLab_class.frame.size.height);
+//    CGSize unitSize = [tempUnit sizeWithFont:[UIFont systemFontOfSize:14]];
+//    cell.mLab_class.frame = CGRectMake(cell.mLab_name.frame.origin.x+cell.mLab_name.frame.size.width, 18, unitSize.width, cell.mLab_class.frame.size.height);
+    cell.mLab_class.frame = CGRectMake(cell.mLab_name.frame.origin.x+cell.mLab_name.frame.size.width, 18, [dm getInstance].width-cell.mLab_name.frame.origin.x-nameSize.width-10, cell.mLab_class.frame.size.height);
     cell.mLab_class.text = tempUnit;
     
     //判断是否隐藏
--- JiaoBao/JiaoBao/views/InternetApplications/workView/WorkView_new2.m
+++ JiaoBao/JiaoBao/views/InternetApplications/workView/WorkView_new2.m
@@ -206,19 +206,20 @@
     cell.mLab_name.frame = CGRectMake(cell.mImgV_head.frame.origin.x+cell.mImgV_head.frame.size.width+10, 15, nameSize.width, 20);
     cell.mLab_name.text = name;
     
-    //单位
-    if (model.UnitShortName.length>0) {
-        cell.mLab_unit.hidden = NO;
-    }
-    NSString *unitName = [NSString stringWithFormat:@"(%@)",model.UnitShortName];
-    CGSize unitSize = [[NSString stringWithFormat:@"%@",unitName] sizeWithFont:[UIFont systemFontOfSize:12]];
-    cell.mLab_unit.frame = CGRectMake(cell.mLab_name.frame.origin.x+nameSize.width, cell.mLab_name.frame.origin.y, unitSize.width, cell.mLab_unit.frame.size.height);
-    cell.mLab_unit.text = unitName;
     //时间
     CGSize timeSize = [[NSString stringWithFormat:@"%@",model.RecDate] sizeWithFont:[UIFont systemFontOfSize:12]];
     cell.mLab_time.frame = CGRectMake([dm getInstance].width-timeSize.width-12, 15, timeSize.width, 20);
     cell.mLab_time.textColor = [UIColor colorWithRed:128/255.0 green:128/255.0 blue:128/255.0 alpha:1];
     cell.mLab_time.text = model.RecDate;
+    
+    //单位
+    if (model.UnitShortName.length>0) {
+        cell.mLab_unit.hidden = NO;
+    }
+    NSString *unitName = [NSString stringWithFormat:@"(%@)",model.UnitShortName];
+//    CGSize unitSize = [[NSString stringWithFormat:@"%@",unitName] sizeWithFont:[UIFont systemFontOfSize:12]];
+    cell.mLab_unit.frame = CGRectMake(cell.mLab_name.frame.origin.x+nameSize.width, cell.mLab_name.frame.origin.y, [dm getInstance].width-cell.mLab_name.frame.origin.x-nameSize.width-timeSize.width-20, cell.mLab_unit.frame.size.height);
+    cell.mLab_unit.text = unitName;
     
     //内容
     cell.mLab_content.text = model.MsgContent;
