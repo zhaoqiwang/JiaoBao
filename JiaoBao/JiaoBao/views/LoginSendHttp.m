1872256844c343295b395186d7f0789cbb30676d
Zqw <578159259@qq.com>
2015-05-09 12:47:59 +0800

    gengxin

--- JiaoBao/JiaoBao/views/LoginSendHttp.m
+++ JiaoBao/JiaoBao/views/LoginSendHttp.m
@@ -1165,6 +1165,7 @@ static LoginSendHttp *loginSendHttp = nil;
                         Identity_UserClasses_model *temp = [model.UserClasses objectAtIndex:0];
                         [dm getInstance].mStr_unit = temp.ClassName;
                         [dm getInstance].uType = [model.RoleIdentity intValue];
+                        [dm getInstance].UID = [temp.ClassID intValue];
                         break;
                     }
                 }
