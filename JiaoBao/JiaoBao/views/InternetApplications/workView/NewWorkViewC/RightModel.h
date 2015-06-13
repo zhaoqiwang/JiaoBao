207a284ba3b6757840204e20f28e4b0114c85d85
songyanming <1062608083@qq.com>
2015-06-13 09:02:17 +0800

    gengxin

---
+++ JiaoBao/JiaoBao/views/InternetApplications/workView/NewWorkViewC/RightModel.h
@@ -0,0 +1,13 @@
+//
+//  RightModel.h
+//  JiaoBao
+//
+//  Created by songyanming on 15/6/13.
+//  Copyright (c) 2015年 JSY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RightModel : NSObject
+
+@end
---
+++ JiaoBao/JiaoBao/views/InternetApplications/workView/NewWorkViewC/RightModel.m
@@ -0,0 +1,13 @@
+//
+//  RightModel.m
+//  JiaoBao
+//
+//  Created by songyanming on 15/6/13.
+//  Copyright (c) 2015年 JSY. All rights reserved.
+//
+
+#import "RightModel.h"
+
+@implementation RightModel
+
+@end
