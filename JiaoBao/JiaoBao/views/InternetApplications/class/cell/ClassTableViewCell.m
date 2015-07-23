fe500e42a9d4844de3ca26d8ee035f419ae8d19a
songyanming <1062608083@qq.com>
2015-07-23 10:31:56 +0800

    更新

--- JiaoBao/JiaoBao/views/InternetApplications/class/cell/ClassTableViewCell.m
+++ JiaoBao/JiaoBao/views/InternetApplications/class/cell/ClassTableViewCell.m
@@ -58,6 +58,12 @@
 //    ClassTableViewCell *classCell = (ClassTableViewCell*)[[tableView superview]superview];
 //    NSLog(@"cell = %@",classCell);
     static NSString *indentifier = @"CommentCell";
+    BOOL nibsRegistered = NO;
+    if (!nibsRegistered) {
+        UINib *nib = [UINib nibWithNibName:NSStringFromClass([CommentCell class]) bundle:nil];
+        [tableView registerNib:nib forCellReuseIdentifier:indentifier];
+        nibsRegistered = YES;
+    }
     CommentCell *cell = (CommentCell *)[tableView dequeueReusableCellWithIdentifier:indentifier];
     if(cell == nil)
     {
