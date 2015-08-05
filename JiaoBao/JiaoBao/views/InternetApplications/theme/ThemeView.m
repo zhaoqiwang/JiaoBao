2201dc03e984ac8e445f32e895bde5bc3986fb60
songyanming <1062608083@qq.com>
2015-08-05 17:04:47 +0800

    更新

--- JiaoBao/JiaoBao/views/InternetApplications/theme/ThemeView.m
+++ JiaoBao/JiaoBao/views/InternetApplications/theme/ThemeView.m
@@ -66,21 +66,21 @@
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString *indentifier = @"TopArthListCell";
-    TopArthListCell *cell = (TopArthListCell *)[tableView dequeueReusableCellWithIdentifier:indentifier];
-    if (cell == nil) {
-        cell = [[TopArthListCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:indentifier];
-        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"TopArthListCell" owner:self options:nil];
-        //这时myCell对象已经通过自定义xib文件生成了
-        if ([nib count]>0) {
-            cell = (TopArthListCell *)[nib objectAtIndex:0];
-            //加判断看是否成功实例化该cell，成功的话赋给cell用来返回。
-        }
-        //添加图片点击事件
-        //若是需要重用，需要写上以下两句代码
-        UINib * n= [UINib nibWithNibName:@"TopArthListCell" bundle:[NSBundle mainBundle]];
-//        [self.mTableV_detail registerNib:n forCellReuseIdentifier:indentifier];
-    }
+//    static NSString *indentifier = @"TopArthListCell";
+//    TopArthListCell *cell = (TopArthListCell *)[tableView dequeueReusableCellWithIdentifier:indentifier];
+//    if (cell == nil) {
+//        cell = [[TopArthListCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:indentifier];
+//        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"TopArthListCell" owner:self options:nil];
+//        //这时myCell对象已经通过自定义xib文件生成了
+//        if ([nib count]>0) {
+//            cell = (TopArthListCell *)[nib objectAtIndex:0];
+//            //加判断看是否成功实例化该cell，成功的话赋给cell用来返回。
+//        }
+//        //添加图片点击事件
+//        //若是需要重用，需要写上以下两句代码
+//        UINib * n= [UINib nibWithNibName:@"TopArthListCell" bundle:[NSBundle mainBundle]];
+////        [self.mTableV_detail registerNib:n forCellReuseIdentifier:indentifier];
+//    }
 //    if (tableView.tag == 1){
     
     return nil;
