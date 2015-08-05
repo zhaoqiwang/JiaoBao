d8b5b47f764b57350444d24f40a69736e80c6b2b
Zqw <578159259@qq.com>
2015-08-05 17:10:38 +0800

    更新

--- JiaoBao/JiaoBao/views/InternetApplications/theme/ThemeView.m
+++ JiaoBao/JiaoBao/views/InternetApplications/theme/ThemeView.m
@@ -11,7 +11,6 @@
 #import "MobClick.h"
 
 @implementation ThemeView
-//@synthesize mScrollV_share,mTableV_detail,mTableV_difine,mInt_index,mArr_tabel,mBtn_add,mLab_name,mArr_difine;
 
 - (id)initWithFrame1:(CGRect)frame{
     self = [super init];
@@ -21,8 +20,60 @@
         //做bug服务器显示当前的哪个界面
         NSString *nowViewStr = [NSString stringWithUTF8String:object_getClassName(self)];
         [[NSUserDefaults standardUserDefaults]setValue:nowViewStr forKey:BUGFROM];
+        [self setBackgroundColor:[UIColor colorWithRed:247/255.0 green:246/255.0 blue:246/255.0 alpha:1]];
+        
+        self.mArr_first = [NSMutableArray array];
+        self.mArr_choice = [NSMutableArray array];
+        self.mArr_education = [NSMutableArray array];
+        self.mArr_extracurricular = [NSMutableArray array];
+        self.mArr_happy = [NSMutableArray array];
+        self.mArr_life = [NSMutableArray array];
+        self.mArr_paternity = [NSMutableArray array];
+        self.mArr_recommend = [NSMutableArray array];
+        self.mArr_science = [NSMutableArray array];
+        self.mInt_index = 0;
+        //首页精选等
+        self.mScrollV_all = [[UIScrollView alloc] initWithFrame:CGRectMake(10, 0, [dm getInstance].width-20-40, 48)];
+        int tempWidth = 50;
+        for (int i=0; i<9; i++) {
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            [button setFrame:CGRectMake(tempWidth*i, 1, tempWidth, 47)];
+            [button setTag:i];
+            if (i==0) {
+                button.selected = YES;
+            }
+            [button setTitleColor:[UIColor colorWithRed:130/255.0 green:129/255.0 blue:130/255.0 alpha:1] forState:UIControlStateNormal];
+            [button setTitleColor:[UIColor colorWithRed:91/255.0 green:178/255.0 blue:57/255.0 alpha:1] forState:UIControlStateSelected];
+            [button setBackgroundColor:[UIColor colorWithRed:247/255.0 green:246/255.0 blue:246/255.0 alpha:1]];
+            //设置标题位置
+            [button setImage:[UIImage imageNamed:[NSString stringWithFormat:@"NewWorkView_click_%d",0]] forState:UIControlStateNormal];
+            [button setImage:[UIImage imageNamed:[NSString stringWithFormat:@"NewWorkView_%d",0]] forState:UIControlStateSelected];
+            
+            [button addTarget:self action:@selector(selectScrollButton:) forControlEvents:UIControlEventTouchUpInside];
+            [self.mScrollV_all addSubview:button];
+        }
+        self.mScrollV_all.contentSize = CGSizeMake(50*9, 48);
+        [self addSubview:self.mScrollV_all];
+        //表格
+        self.mTableV_knowledge = [[UITableView alloc] initWithFrame:CGRectMake(0, 48, [dm getInstance].width, self.frame.size.height-48)];
+        self.mTableV_knowledge.delegate = self;
+        self.mTableV_knowledge.dataSource = self;
+        [self addSubview:self.mTableV_knowledge];
     }
     return self;
+}
+
+-(void)selectScrollButton:(UIButton *)btn{
+    self.mInt_index = (int)btn.tag;
+    for (UIButton *btn1 in self.mScrollV_all.subviews) {
+        if ([btn1.class isSubclassOfClass:[UIButton class]]) {
+            if ((int)btn1.tag == self.mInt_index) {
+                btn1.selected = YES;
+            }else{
+                btn1.selected = NO;
+            }
+        }
+    }
 }
 
 -(void)ProgressViewLoad{
@@ -30,18 +81,9 @@
     if ([self checkNetWork]) {
         return;
     }
-    [[KnowledgeHttp getInstance]QuestionIndexWithNumPerPage:@"20" pageNum:@"1" CategoryId:@"15"];
-//    [[KnowledgeHttp getInstance]UpdateQuestionWithTabIDStr:@"15" KnContent:@"2 + 2 = ?" TagsList:@""];没成功 TabIDStr参数不知道在哪获取
-//    [[KnowledgeHttp getInstance]NewQuestionWithCategoryId:@"15" Title:@"加法" KnContent:@"1 + 1 = ?" TagsList:@"加,减,乘,除" QFlag:@"1" AreaCode:@"" atAccIds:@""];
-//    [[KnowledgeHttp getInstance]CategoryIndexQuestionWithNumPerPage:@"10" pageNum:@"1" RowCount:@"0" flag:@"1" uid:@"15"];
-    //[[KnowledgeHttp getInstance]GetAllCategory];
-    //[[KnowledgeHttp getInstance]GetCategoryWithParentId:nil subject:nil];
-    //[[KnowledgeHttp getInstance]GetUserInfo];
-//    [[KnowledgeHttp getInstance] knowledgeHttpGetProvice];
-//    [[KnowledgeHttp getInstance]GetAccIdbyNickname:@[@"123",@"456"]];
-    
+    [[KnowledgeHttp getInstance] knowledgeHttpGetProvice];
 //    [[KnowledgeHttp getInstance] knowledgeHttpGetCity:@"" level:@""];
-    //[MBProgressHUD showMessage:@"" toView:self];
+//    [MBProgressHUD showMessage:@"" toView:self];
 }
 //检查当前网络是否可用
 -(BOOL)checkNetWork{
@@ -60,9 +102,44 @@
     [dm getInstance].mImt_shareUnRead = 0;
 }
 
+-(NSMutableArray *)arrayDataSource{
+    NSMutableArray *array = [NSMutableArray array];
+    switch (self.mInt_index) {
+        case 0:
+            array = [NSMutableArray arrayWithArray:self.mArr_first];
+            break;
+        case 1:
+            array = [NSMutableArray arrayWithArray:self.mArr_choice];
+            break;
+        case 2:
+            array = [NSMutableArray arrayWithArray:self.mArr_recommend];
+            break;
+        case 3:
+            array = [NSMutableArray arrayWithArray:self.mArr_education];
+            break;
+        case 4:
+            array = [NSMutableArray arrayWithArray:self.mArr_science];
+            break;
+        case 5:
+            array = [NSMutableArray arrayWithArray:self.mArr_life];
+            break;
+        case 6:
+            array = [NSMutableArray arrayWithArray:self.mArr_happy];
+            break;
+        case 7:
+            array = [NSMutableArray arrayWithArray:self.mArr_paternity];
+            break;
+        case 8:
+            array = [NSMutableArray arrayWithArray:self.mArr_extracurricular];
+            break;
+        default:
+            break;
+    }
+    return array;
+}
+
 -(NSInteger) tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section{
-//        return self.mArr_tabel.count;
-    return 0;
+    return [self arrayDataSource].count;
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -81,17 +158,11 @@
 //        UINib * n= [UINib nibWithNibName:@"TopArthListCell" bundle:[NSBundle mainBundle]];
 ////        [self.mTableV_detail registerNib:n forCellReuseIdentifier:indentifier];
 //    }
-//    if (tableView.tag == 1){
-    
+    NSMutableArray *array = [self arrayDataSource];
     return nil;
 }
 
 -(CGFloat)tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath{
-    if (indexPath.row == 1) {
-        return 20;
-    }else{
-        return 50;
-    }
     return 0;
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
