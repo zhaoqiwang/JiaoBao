85ffb03f12c15ed3ed75c0aebe9ef8a6a03c9194
songyanming <1062608083@qq.com>
2015-10-16 08:37:25 +0800

    更新

---
+++ JiaoBao/JiaoBao/views/InternetApplications/onlineJob/OnlineJobHttp.h
@@ -0,0 +1,13 @@
+//
+//  OnlineJobHttp.h
+//  JiaoBao
+//
+//  Created by songyanming on 15/10/15.
+//  Copyright © 2015年 JSY. All rights reserved.
+//在线作业接口
+
+#import <Foundation/Foundation.h>
+
+@interface OnlineJobHttp : NSObject
+
+@end
---
+++ JiaoBao/JiaoBao/views/InternetApplications/onlineJob/OnlineJobHttp.m
@@ -0,0 +1,13 @@
+//
+//  OnlineJobHttp.m
+//  JiaoBao
+//
+//  Created by songyanming on 15/10/15.
+//  Copyright © 2015年 JSY. All rights reserved.
+//
+
+#import "OnlineJobHttp.h"
+
+@implementation OnlineJobHttp
+
+@end
