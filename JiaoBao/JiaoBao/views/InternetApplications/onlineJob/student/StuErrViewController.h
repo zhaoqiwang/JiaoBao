92e79b60b52b50ca4bda5bf875e9fe91feb0f15a
SongYanming <yebukeji2@163.com>
2016-03-18 10:26:20 +0800

    更新

---
+++ JiaoBao/JiaoBao/views/InternetApplications/onlineJob/student/StuErrViewController.h
@@ -0,0 +1,13 @@
+//
+//  StuErrViewController.h
+//  JiaoBao
+//
+//  Created by SongYanming on 16/3/18.
+//  Copyright © 2016年 JSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StuErrViewController : UIViewController
+
+@end
