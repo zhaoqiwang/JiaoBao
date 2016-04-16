bd73cc917bbfef02801c5ea81ed05a1a0a790fcb
SongYanming <yebukeji2@163.com>
2016-04-16 10:32:17 +0800

    几审为<null>时的处理

--- JiaoBao/JiaoBao/views/InternetApplications/leave/model/LevelNoteModel.m
+++ JiaoBao/JiaoBao/views/InternetApplications/leave/model/LevelNoteModel.m
@@ -17,61 +17,66 @@
     NSDictionary *dic = [dicStr objectFromJSONString];
 
     NSString *Astr = [dic objectForKey:@"A"];
-    if(![Astr isEqualToString:@""]||![Astr isEqual:[NSNull null]]){
+    if([Astr isEqualToString:@""]||[Astr isEqual:[NSNull null]]){
+        self.A = @"一审";
+
+    }else{
         self.A = Astr;
         if([self.A isEqualToString:@"False"])
         {
             self.A = nil;
         }
-    }else{
-        self.A = @"一审";
     }
     NSString *Bstr = [dic objectForKey:@"B"];
-    if(![Bstr isEqualToString:@""]||![Bstr isEqual:[NSNull null]]){
+    if([Bstr isEqualToString:@""]||[Bstr isEqual:[NSNull null]]){
+        self.B = @"二审";
+
+    }else{
         self.B = Bstr;
         if([self.B isEqualToString:@"False"])
         {
             self.B = nil;
         }
-    }else{
-        self.B = @"二审";
     }
     NSString *Cstr = [dic objectForKey:@"C"];
-    if(![Cstr isEqualToString:@""]||![Cstr isEqual:[NSNull null]]){
+    if([Cstr isEqualToString:@""]||[Cstr isEqual:[NSNull null]]){
+        self.C = @"三审";
+
+    }else{
         self.C = Cstr;
         if([self.C isEqualToString:@"False"])
         {
             self.C = nil;
         }
-    }else{
-        self.C = @"三审";
     }
     NSString *Dstr = [dic objectForKey:@"D"];
-    if(![Dstr isEqualToString:@""]||![Dstr isEqual:[NSNull null]]){
+    if([Dstr isEqualToString:@""]||[Dstr isEqual:[NSNull null]]){
+        self.D = @"四审";
+
+    }else{
         self.D = Dstr;
         if([self.D isEqualToString:@"False"])
         {
             self.D = nil;
         }
-    }else{
-        self.D = @"四审";
     }
     NSString *Estr = [dic objectForKey:@"E"];
-    if(![Estr isEqualToString:@""]||![Estr isEqual:[NSNull null]]){
+    if([Estr isEqualToString:@""]||[Estr isEqual:[NSNull null]]){
+        self.E = @"五审";
+
+    }else{
         self.E = Estr;
         if([self.E isEqualToString:@"False"])
         {
             self.E = nil;
         }
-    }else{
-        self.E = @"五审";
-    }
 
 //    self.A = [dic objectForKey:@"A"];
 //    self.B = [dic objectForKey:@"B"];
 //    self.C = [dic objectForKey:@"C"];
 //    self.D = [dic objectForKey:@"D"];
 //    self.E = [dic objectForKey:@"E"];
+}
 }
 
 @end
